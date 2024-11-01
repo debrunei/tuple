dd8e878f68108e7683067fa8eb7b61be728517bc
debrunei000 <debrunei000@warren.k12.in.us>
2024-11-01 10:13:18 -0400

    f

---
+++ c.py
@@ -0,0 +1,17 @@
+#SECTION 1
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+print(fruit_tuple[-1])
+slice_tuple = fruit_tuple[1:2+1]
+print(slice_tuple)
+#SECTION 2
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+print(num_tuple.count(3))
+print(num_tuple.index(8))
+
+#SECTION 3
+person_info = ("Alice", 28, "Engineer")
+name, number, boolean = person_info
+print(name)
+print(number)
+print(boolean)
